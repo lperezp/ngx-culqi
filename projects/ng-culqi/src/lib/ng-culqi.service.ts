505c1106ba27a43e6cf2b4dd0807a4bba8f994c2
Luis Eduardo <luiseperezp.10@gmail.com>
2021-08-08 20:30:53 -0500

    feat(lib): update lib

--- projects/ng-culqi/src/lib/ng-culqi.service.ts
+++ projects/ng-culqi/src/lib/ng-culqi.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
+import { ICulqiSettings } from './models/culqi.model';
+
+export declare let Culqi: any;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgCulqiService {
+  TOKEN_CULQI = '';
 
-  constructor() { }
+  constructor() {
+    document.addEventListener('payment_event', (token: any) => {
+      this.TOKEN_CULQI = token.detail;
+      console.log(this.TOKEN_CULQI);
+    });
+  }
+
+  initCulqi(CULQI_PUBLIC_KEY: string): void {
+    Culqi.publicKey = CULQI_PUBLIC_KEY;
+  }
+
+  open(): void {
+    Culqi.open();
+  }
+
+  payOrder(culqiSettings: ICulqiSettings): void {
+    Culqi.settings(culqiSettings);
+
+    Culqi.options({
+      lang: 'auto',
+      modal: true,
+      installments: false,
+      customButton: '',
+    });
+    Culqi.open();
+  }
 }
--- projects/ng-culqi/src/public-api.ts
+++ projects/ng-culqi/src/public-api.ts
@@ -3,5 +3,4 @@
  */
 
 export * from './lib/ng-culqi.service';
-export * from './lib/ng-culqi.component';
 export * from './lib/ng-culqi.module';
