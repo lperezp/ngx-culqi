edb3b549f287d15c115befd44f4b55dfb9d55242
Luis Eduardo <luiseperezp.10@gmail.com>
2021-08-08 21:28:14 -0500

    fix(lib): delete function by events

--- projects/ng-culqi/src/lib/ng-culqi.service.ts
+++ projects/ng-culqi/src/lib/ng-culqi.service.ts
@@ -8,20 +8,8 @@ export declare let Culqi: any;
 })
 export class NgCulqiService {
   TOKEN_CULQI = '';
-
-  constructor() {
-    document.addEventListener('payment_event', (token: any) => {
-      this.TOKEN_CULQI = token.detail;
-      console.log(this.TOKEN_CULQI);
-    });
-  }
-
   initCulqi(CULQI_PUBLIC_KEY: string): void {
     Culqi.publicKey = CULQI_PUBLIC_KEY;
-  }
-
-  open(): void {
-    Culqi.open();
   }
 
   generateToken(
@@ -37,6 +25,10 @@ export class NgCulqiService {
     };
     culqiOptions ? Culqi.options(culqiOptions) : Culqi.options(culqiOptionsAux);
     Culqi.options();
+    Culqi.open();
+  }
+
+  open(): void {
     Culqi.open();
   }
 }
